bc49d9a77a5c2cd078e0a41dfd8de55a03fd3789
YasakaShun <sdshvedujbfxye@gmail.com>
2018-06-24 21:48:11 +0900

    Form Chart でのグラフ表示テスト

--- TaskTimer/MainWindow.xaml.cs
+++ TaskTimer/MainWindow.xaml.cs
@@ -7,6 +7,8 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Windows.Forms.Integration;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -23,6 +25,29 @@ namespace TaskTimer
         public MainWindow()
         {
             InitializeComponent();
+
+            var windowsFormsHost = (WindowsFormsHost)MainGrid.Children[0];
+
+            var chart = (Chart)windowsFormsHost.Child;
+
+            // ChartArea追加
+            {
+                var area = new ChartArea("Area1");
+                area.AlignmentOrientation = AreaAlignmentOrientations.Vertical;
+                area.Area3DStyle.Enable3D = true;
+                chart.ChartAreas.Add(area);
+            }
+
+            // Seriesの作成と値の追加
+            Series series = new Series();
+            series.ChartType = SeriesChartType.Bar;
+
+            for (int i = 0; i < 5; ++i)
+            {
+                series.Points.AddXY(i, 1 + i);
+            }
+
+            chart.Series.Add(series);
         }
     }
 }
