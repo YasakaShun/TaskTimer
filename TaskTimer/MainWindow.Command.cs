f850282b5011d790de04de3860eac20b0c71de21
YasakaShun <sdshvedujbfxye@gmail.com>
2018-07-21 16:18:41 +0900

    RoutedUICommand を使う形でコマンドを実装しなおし

--- TaskTimer/MainWindow.Command.cs
+++ TaskTimer/MainWindow.Command.cs
@@ -4,15 +4,103 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Input;
 
 namespace TaskTimer
 {
     // コマンド定義用クラス
     public partial class MainWindow
     {
-        private void CommandBinding_Executed(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        public static readonly RoutedUICommand FileLoad = new RoutedUICommand();
+        public static readonly RoutedUICommand FileSave = new RoutedUICommand();
+        public static readonly RoutedUICommand AddItem = new RoutedUICommand();
+        public static readonly RoutedUICommand DoneItem = new RoutedUICommand();
+
+        /// <summary>
+        /// ファイル読み込み
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FileLoad_Execute(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
         {
-            System.Windows.MessageBox.Show("コマンドが実行されました");
+            var dialog = new OpenFileDialog();
+            dialog.Title = "ファイルを開く";
+            dialog.Filter = "CSVファイル(*.csv)|*.csv";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                try
+                {
+                    using (var sr = new System.IO.StreamReader(dialog.FileName, System.Text.Encoding.UTF8))
+                    {
+                        mViewModel.Items.Clear();
+
+                        while (!sr.EndOfStream)
+                        {
+                            mViewModel.Items.Add(ViewModel.Item.Parse(sr.ReadLine()));
+                        }
+                    }
+                }
+                catch (SystemException exception)
+                {
+                    System.Windows.MessageBox.Show(exception.Message);
+                }
+            }
+        }
+
+        /// <summary>
+        /// ファイル保存
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FileSave_Execute(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            var dialog = new SaveFileDialog();
+            dialog.Title = "ファイルを保存";
+            dialog.Filter = "CSVファイル|*.csv";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                try
+                {
+                    using (var sw = new System.IO.StreamWriter(dialog.FileName, false, System.Text.Encoding.UTF8))
+                    {
+                        foreach (var item in mViewModel.Items)
+                        {
+                            sw.WriteLine(item.ToString());
+                        }
+                    }
+                }
+                catch (SystemException exception)
+                {
+                    System.Windows.MessageBox.Show(exception.Message);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 空の項目を追加
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddItem_Execute(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            var itemCollection = e.Parameter as ViewModel.ItemCollection;
+
+            itemCollection.Add(new ViewModel.Item());
+        }
+
+        /// <summary>
+        /// 指定アイテムを消化
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DoneItem_Execute(object sender, System.Windows.Input.ExecutedRoutedEventArgs e)
+        {
+            var item = e.Parameter as ViewModel.Item;
+            if (item != null)
+            {
+                item.Done();
+            }
         }
     }
 }
--- TaskTimer/ViewModel/Obj.cs
+++ TaskTimer/ViewModel/Obj.cs
@@ -11,11 +11,9 @@ namespace TaskTimer.ViewModel
         public Obj()
         {
             Items = new ItemCollection();
-            Commands = new Commands(this);
         }
 
         public ItemCollection Items { get; set; }
-        public Commands Commands { get; set; }
     }
 
 }
